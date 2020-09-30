c46335875356fc9b8cc3cdb47a32573b5617ea1c
limheonjun <jumong40@naver.com>
2020-09-30 20:09:15 +0900

    커스텀 훅 추가

---
+++ hooks/useInput.tsx
@@ -0,0 +1,11 @@
+import { useState, useCallback } from 'react'
+
+export default function (initialValue) {
+  const [value, setValue] = useState(initialValue)
+  const handler = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    //e.target.value 무조건 string타입이기 때문에 function<T> 제네릭으로 useState의 제네릭을 설정할 수 없음, 
+    //혹여나 string외의 다른 타입이 들어오면 e.target.value가 해당 타입이 아니기 때문
+    setValue(e.target.value)
+  }, [])
+  return [value, handler]
+}
