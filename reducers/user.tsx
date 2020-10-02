9e51fbc7f914d2e4c4e02a1a14066d65362f724d
limheonjun <jumong40@naver.com>
2020-10-02 12:56:04 +0900

    리듀서 코드 수정

--- reducers/user.tsx
+++ reducers/user.tsx
@@ -1,36 +1,90 @@
 export const initalState = {
+  isLoggingIn: false, //로그인 시도중
   isLoggedIn: false,
+  isLoggingOut: false, //로그아웃 시도중
   me: null,
   signUpData: {},
   loginData: {}
 }
 
-
 // action creator
-export const loginAction = (data) => {
+export const loginRequestAction = (data) => {
   return {
-    type: 'LOG_IN',
+    type: 'LOG_IN_REQUEST',
     data,
   }
 }
 
-export const logoutAction = () => {
+// Saga가 put을 통해 알아서 호출
+// export const loginSuccessAction = (data) => {
+//   return {
+//     type: 'LOG_IN_SUCCESS',
+//     data,
+//   }
+// }
+
+// export const loginFailureAction = (data) => {
+//   return {
+//     type: 'LOG_IN_FAILURE',
+//     data,
+//   }
+// }
+
+export const logoutRequestAction = () => {
   return {
-    type: 'LOG_OUT',
+    type: 'LOG_OUT_REQUEST',
+  }
+}
+
+export const logoutSuccessAction = () => {
+  return {
+    type: 'LOG_OUT_SUCCESS',
+  }
+}
+
+export const logoutFailureAction = () => {
+  return {
+    type: 'LOG_OUT_FAILURE',
   }
 }
 
 const reducer = (state = initalState, action) => {
   switch (action.type) {
-    case 'LOG_IN':
+    case 'LOG_IN_REQUEST':
       return {
         ...state,
-        isLoggedIn: true,
-        me: action.data,
+        isLoggingIn: true,
       }
-    case 'LOG_OUT':
+    case 'LOG_IN_SUCCESS':
       return {
         ...state,
+        isLoggingIn: false,
+        isLoggedIn: true,
+        me: { ...action.data, nickname: 'zerocho' },
+      }
+    case 'LOG_IN_FAILURE':
+      return {
+        ...state,
+        isLoggingIn: false,
+        isLoggedIn: false,
+      }
+    case 'LOG_OUT_REQUEST':
+      return {
+        ...state,
+        isLoggingOut: true,
+        me: null,
+      }
+    case 'LOG_OUT_SUCCESS':
+      return {
+        ...state,
+        isLoggingOut: false,
+        isLoggedIn: false,
+        me: null,
+    }
+    case 'LOG_OUT_FAILURE':
+      return {
+        ...state,
+        isLoggingOut: false,
         isLoggedIn: false,
         me: null,
       }
