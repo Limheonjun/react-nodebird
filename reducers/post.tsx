9f99c62e02ba36784ca497866125dcc646410bc3
limheonjun <jumong40@naver.com>
2020-10-02 14:55:01 +0900

    게시글, 댓글 Saga 작성

--- reducers/post.tsx
+++ reducers/post.tsx
@@ -1,3 +1,5 @@
+import shortId from 'shortid'
+
 export const initalState = {
   mainPosts: [{
     id: 1,
@@ -29,6 +31,9 @@ export const initalState = {
   addPostLoading: false,
   addPostDone: false,
   addPostError: null,
+  addCommentLoading: false,
+  addCommentDone: false,
+  addCommentError: null,
 }
 
 export const ADD_POST_REQUEST = 'ADD_POST_REQUEST';
@@ -46,19 +51,28 @@ export const addPost = (data) => ({
 
 export const addComment = (data) => ({
   type: ADD_COMMENT_REQUEST,
-  data,
+  data, 
 })
 
-const dummyPost = {
-  id: 2,
-  content: '더미데이터입니다.',
+const dummyPost = (data) => ({
+  id: shortId.generate(),
+  content: data,
   User: {
     id: 1,
     nickname: '제로초',
   },
   Images: [],
   Comments: [],
-};
+});
+
+const dummyComment = (data) => ({
+  id: shortId.generate(),
+  content: data,
+  User: {
+    id: 1,
+    nickname: '제로초',
+  },
+});
 
 const reducer = (state = initalState, action) => {
   switch (action.type) {
@@ -72,7 +86,7 @@ const reducer = (state = initalState, action) => {
     case ADD_POST_SUCCESS:
       return {
         ...state,
-        mainPosts: [dummyPost, ...state.mainPosts],
+        mainPosts: [dummyPost(action.data), ...state.mainPosts],
         addPostLoading: false,
         addPostDone: true,
       }
@@ -89,13 +103,19 @@ const reducer = (state = initalState, action) => {
         addCommentDone: false,
         addCommentError: null,
       }
-    case ADD_COMMENT_SUCCESS:
+    case ADD_COMMENT_SUCCESS: {
+      const postIndex = state.mainPosts.findIndex((v) => (v.id === action.data.postId))
+      const post = state.mainPosts[postIndex]
+      post.Comments = [dummyComment(action.data.content), ...post.Comments]
+      const mainPosts = [...state.mainPosts]
+      mainPosts[postIndex] = post
       return {
         ...state,
-        mainPosts: [dummyPost, ...state.mainPosts],
+        mainPosts,
         addCommentLoading: false,
         addCommentDone: true,
       }
+    }
     case ADD_COMMENT_FAILURE:
       return {
         ...state,
