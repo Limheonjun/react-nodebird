6c40b2fa8674ccd5273ac5c14258aadf109a2b54
limheonjun <jumong40@naver.com>
2020-10-09 20:37:48 +0900

    백엔드 서버 포트 변경

--- sagas/index.tsx
+++ sagas/index.tsx
@@ -3,7 +3,7 @@ import postSaga from './post';
 import userSaga from './user';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:3065'
+axios.defaults.baseURL = 'http://localhost:8080'
 
 export default function* rootSaga() {
   yield all([
