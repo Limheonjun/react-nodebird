5ce9bebd000c59f19e9fae4a39bb171d09871094
limheonjun <jumong40@naver.com>
2020-10-11 19:00:31 +0900

    로그인 세션 유지시 내 정보 불러오기

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,12 +4,16 @@ import { useSelector, useDispatch } from 'react-redux';
 import PostCard from '../components/PostCard';
 import PostForm from '../components/PostForm';
 import { LOAD_POSTS_REQUEST } from '../reducers/post';
+import { LOAD_MY_INFO_REQUEST} from '../reducers/user';
 
 const Home = function () {
   const { me } = useSelector(state => state.user)
   const { mainPosts, hasMorePosts, loadPostsLoading } = useSelector(state => state.post)
   const dispatch = useDispatch()
   useEffect(() => {
+    dispatch({
+      type: LOAD_MY_INFO_REQUEST,
+    }),
     dispatch({
       type: LOAD_POSTS_REQUEST,
     })
