726e23fbecca8a6444e564b9279002cc3f3ffa7f
limheonjun <jumong40@naver.com>
2020-10-13 20:18:36 +0900

    로그인 실패시 에러메시지 출력

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useMemo, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { Form, Input, Button } from 'antd'
 import Link from 'next/link'
 import styled from 'styled-components'
@@ -17,12 +17,18 @@ const FormWrapper = styled(Form)`
 
 const LoginForm = function () {
   const dispatch = useDispatch()
-  const { logInLoading } = useSelector((state => state.user))
+  const { logInLoading,   logInError } = useSelector((state => state.user))
   const [email, onChangeEmail] = useInput('')
   const [password, onChangePassword] = useInput('')
   const [passwordCheck, setPasswordCheck] = useState<string>('')
 
   const style = useMemo(() => ({ marginTop: 10 }), [])
+
+  useEffect(() => {
+    if (logInError) {
+      alert(logInError)
+    }
+  }, [logInError])
   
   const onSubmitForm = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
     console.log(email, password);
